b0c6f7803520f6d90fc31460ae8e6d62d21ec8e1
Gustavo Rodrigues <guhrodriguesads@gmail.com>
2023-06-06 13:50:54 -0300

    feat: cria app confeitaria com suas telas e teste

---
+++ src/br/newtonpaiva/ui/Teste.java
@@ -0,0 +1,13 @@
+package br.newtonpaiva.ui;
+
+import br.newtonpaiva.domain.Telas;
+
+import javax.swing.*;
+
+public class Teste extends JFrame{
+
+    public static void main(String[] args) {
+        Telas telas = new Telas();
+        telas.tela1();
+    }
+}
